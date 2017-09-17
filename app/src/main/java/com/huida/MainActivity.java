39a65092285d37faeeb41e7cc58dfebbbd00abde
liling00 <15833209912@163.com>
2017-09-17 11:14:51 +0800

    aaa123123

--- app/src/main/java/com/huida/MainActivity.java
+++ app/src/main/java/com/huida/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+//,
         //我是韩超宇，is sb,yes,
         //dfdfd
     }
