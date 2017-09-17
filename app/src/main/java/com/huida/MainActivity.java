2a9d3ad59982e4706f430863c3cfb76b69259eb9
liuruoshi <1819359186@qq.com>
2017-09-17 10:34:59 +0800

    11

--- app/src/main/java/com/huida/MainActivity.java
+++ app/src/main/java/com/huida/MainActivity.java
@@ -8,7 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //123,
+        //123,,
         setContentView(R.layout.activity_main);
         //我是韩超宇，is sb,yes,
         //dfdfd
